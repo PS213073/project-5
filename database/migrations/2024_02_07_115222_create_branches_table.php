3cb181c7650c8681eeee8e514c36081b123b6bc6
ps227374 <ps227374@summacollege.nl>
2024-02-07 14:43:26 +0100

    migrations uitgebreid

--- database/migrations/2024_02_07_115222_create_branches_table.php
+++ database/migrations/2024_02_07_115222_create_branches_table.php
@@ -14,6 +14,11 @@ return new class extends Migration
         Schema::create('branches', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->text('postal_code');
+            $table->text('address');
+            $table->integer('telephone_number');
+            $table->text('email_adress');
+            $table->text('name');
         });
     }
 
--- database/migrations/2024_02_07_115257_create_contact_forms_table.php
+++ database/migrations/2024_02_07_115257_create_contact_forms_table.php
@@ -14,6 +14,10 @@ return new class extends Migration
         Schema::create('contact_forms', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->integer('telephone_number');
+            $table->text('email_address');
+            $table->text('invoice_number')->nullable();
+            $table->text('message');
         });
     }
 
