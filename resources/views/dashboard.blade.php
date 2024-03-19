a7244beb364c1630f59de8b1f11bb5ad0e142e2a
PS200824 <93642263+PS200824@users.noreply.github.com>
2024-03-19 15:33:34 +0100

    Dasborad design implemented

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,19 +1,20 @@
 <x-app-layout>
     <div>
-         <main class="flex-1 overflow-x-hidden overflow-y-auto bg-gray-200">
-             <div class="container mx-auto px-6 py-8">
+        <main class="flex-1 overflow-x-hidden overflow-y-auto bg-gray-200">
+            <div
+                class="flex flex-col justify-center items-center container mx-auto md:container md:mx-[0%] md:w-full px-6 py-8 bg-[#3E6553] ">
 
 
-                 <h3 class="text-gray-700 text-3xl font-medium">Welcome : {{ auth()->user()->name }}</h3>
+                <h3 class="text-white text-3xl font-medium">Welcome : {{ auth()->user()->name }}</h3>
 
-                 <p>Role : <b>
-                     @foreach(auth()->user()->roles as $role)
-                         {{ $role->name }}
-                     @endforeach
-                 </b> </p>
+                <p>Role : <b>
+                        @foreach (auth()->user()->roles as $role)
+                            {{ $role->name }}
+                        @endforeach
+                    </b> </p>
 
-             </div>
-         </main>
-     </div>
- </div>
- </x-app-layout>
+            </div>
+        </main>
+    </div>
+    </div>
+</x-app-layout>
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -13,6 +13,7 @@
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
     <!-- Scripts -->
+    {{-- @vite('resources/css/app.css') --}}
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
