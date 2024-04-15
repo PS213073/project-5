cdb1081090a98f0cf79774fba4288b42a22ed9b0
Daniel Shahzada <107118950+PS213073@users.noreply.github.com>
2024-04-15 13:36:09 +0200

    english works in admin dashboard changed to dutch

--- resources/views/setting/user/new.blade.php
+++ resources/views/setting/user/new.blade.php
@@ -7,7 +7,7 @@
                    @csrf
                    @method('post')
                    <div class="flex flex-col space-y-2">
-                     <label for="name" class="text-gray-700 select-none font-medium">User Name</label>
+                     <label for="name" class="text-gray-700 select-none font-medium">Gebruiker Naam</label>
                      <input id="name" type="text" name="name" value="{{ old('name') }}"
                        placeholder="Enter name" class="px-4 py-2 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-gray-200"
                      />
@@ -21,19 +21,19 @@
                  </div>
 
                  <div class="flex flex-col space-y-2">
-                     <label for="password" class="text-gray-700 select-none font-medium">Password</label>
+                     <label for="password" class="text-gray-700 select-none font-medium">Wachtwoord</label>
                      <input id="password" type="password" name="password" value="{{ old('password') }}"
                        placeholder="Enter password" class="px-4 py-2 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-gray-200"
                      />
                  </div>
 
                  <div class="flex flex-col space-y-2">
-                     <label for="password_confirmation" class="text-gray-700 select-none font-medium">Confirm Password</label>
+                     <label for="password_confirmation" class="text-gray-700 select-none font-medium">Bevestig Wachtwoord</label>
                      <input id="password_confirmation" type="password" name="password_confirmation" placeholder="Re-enter password" class="px-4 py-2 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-gray-200"
                      />
                  </div>
 
-                 <h3 class="text-xl my-4 text-gray-600">Role</h3>
+                 <h3 class="text-xl my-4 text-gray-600">Rol</h3>
                  <div class="grid grid-cols-3 gap-4">
                    @foreach($roles as $role)
                        <div class="flex flex-col justify-cente">
@@ -47,7 +47,7 @@
                    @endforeach
                  </div>
                  <div class="text-center mt-16 mb-16">
-                   <button type="submit" class="bg-blue-500 text-white font-bold px-5 py-1 rounded focus:outline-none shadow hover:bg-blue-500 transition-colors ">Submit</button>
+                   <button type="submit" class="bg-blue-500 text-white font-bold px-5 py-1 rounded focus:outline-none shadow hover:bg-blue-500 transition-colors ">Opslaan</button>
                  </div>
                </div>
 
