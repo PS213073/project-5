d49486ad4d51343152b34ce5090a2b8dc05e5082
PS200824 <93642263+PS200824@users.noreply.github.com>
2024-04-19 11:31:36 +0200

    Font is applied to all pages

--- resources/views/front/dashboard.blade.php
+++ resources/views/front/dashboard.blade.php
@@ -1,6 +1,6 @@
 <x-front-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        <h2 class="font-semibold text-xl text-center text-gray-800 leading-tight">
             {{ __('Dashboard') }}
         </h2>
     </x-slot>
