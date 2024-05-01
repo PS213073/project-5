3498af0262329d9ae7761118b1c95084b2df33fe
Daniel Shahzada <107118950+PS213073@users.noreply.github.com>
2024-05-01 22:38:52 +0200

    Revert "Merge pull request #53 from PS213073/bushra"
    This reverts commit ad8a850030aa1e9c7f7a2ebc8039cca9df8bd0e4, reversing
    changes made to be7437ab55354587c247fc645c955686b329c9b7.

--- resources/views/front/profile/partials/delete-user-form.blade.php
+++ resources/views/front/profile/partials/delete-user-form.blade.php
@@ -1,16 +1,18 @@
 <section class="space-y-6">
     <header>
         <h2 class="text-lg font-medium text-gray-900">
-            {{ __('Account Verwijderen') }}
+            {{ __('Delete Account') }}
         </h2>
 
         <p class="mt-1 text-sm text-gray-600">
-            {{ __('Zodra uw account is verwijderd, worden alle bronnen en gegevens permanent verwijderd. Voordat u uw account verwijdert, downloadt u alle gegevens of informatie die u wilt behouden.') }}
+            {{ __('Once your account is deleted, all of its resources and data will be permanently deleted. Before deleting your account, please download any data or information that you wish to retain.') }}
         </p>
     </header>
 
-    <x-danger-button x-data=""
-        x-on:click.prevent="$dispatch('open-modal', 'confirm-user-deletion')">{{ __('Account Verwijderen') }}</x-danger-button>
+    <x-danger-button
+        x-data=""
+        x-on:click.prevent="$dispatch('open-modal', 'confirm-user-deletion')"
+    >{{ __('Delete Account') }}</x-danger-button>
 
     <x-modal name="confirm-user-deletion" :show="$errors->userDeletion->isNotEmpty()" focusable>
         <form method="post" action="{{ route('front.profile.destroy') }}" class="p-6">
@@ -18,29 +20,34 @@
             @method('delete')
 
             <h2 class="text-lg font-medium text-gray-900">
-                {{ __('Weet u zeker dat u uw account wilt verwijderen?') }}
+                {{ __('Are you sure you want to delete your account?') }}
             </h2>
 
             <p class="mt-1 text-sm text-gray-600">
-                {{ __('Zodra uw account is verwijderd, worden alle bronnen en gegevens permanent verwijderd. Voer uw wachtwoord in om te bevestigen dat u uw account definitief wilt verwijderen.') }}
+                {{ __('Once your account is deleted, all of its resources and data will be permanently deleted. Please enter your password to confirm you would like to permanently delete your account.') }}
             </p>
 
             <div class="mt-6">
-                <x-input-label for="password" value="{{ __('Wachtwoord') }}" class="sr-only" />
+                <x-input-label for="password" value="{{ __('Password') }}" class="sr-only" />
 
-                <x-text-input id="password" name="password" type="password" class="mt-1 block w-3/4"
-                    placeholder="{{ __('Wachtwoord') }}" />
+                <x-text-input
+                    id="password"
+                    name="password"
+                    type="password"
+                    class="mt-1 block w-3/4"
+                    placeholder="{{ __('Password') }}"
+                />
 
                 <x-input-error :messages="$errors->userDeletion->get('password')" class="mt-2" />
             </div>
 
             <div class="mt-6 flex justify-end">
                 <x-secondary-button x-on:click="$dispatch('close')">
-                    {{ __('Annuleren') }}
+                    {{ __('Cancel') }}
                 </x-secondary-button>
 
                 <x-danger-button class="ms-3">
-                    {{ __('Account Verwijderen') }}
+                    {{ __('Delete Account') }}
                 </x-danger-button>
             </div>
         </form>
